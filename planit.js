2ff87e42b3fcb0fcc279d10a01049263a960cb39
Ubuntu <azureuser@myplanit.myplanit.g3.internal.cloudapp.net>
2015-09-20 05:46:51 +0000

    initial commit

---
+++ planit.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+  $('#menuIcon').click(function(){
+      $(body).prepend('<div class="leftMenu" id="leftContainer"></div>');
+      $('#leftContainer').
+  });
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
