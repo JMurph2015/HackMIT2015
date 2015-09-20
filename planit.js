53ce54a49b27082dc43319ef42612bd56d1360c5
Ubuntu <azureuser@myplanit.myplanit.g3.internal.cloudapp.net>
2015-09-20 06:09:02 +0000

    Getting a deployment together.

--- planit.js
+++ planit.js
@@ -1,7 +1,6 @@
 if (Meteor.isClient) {
   $('#menuIcon').click(function(){
       $(body).prepend('<div class="leftMenu" id="leftContainer"></div>');
-      $('#leftContainer').
   });
 }
 if (Meteor.isServer) {
